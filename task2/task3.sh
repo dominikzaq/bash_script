eb3aba8aaee8ce6d9d291d6b9670008ff1a2e8f7
Dominik Kieltyka <dominikieltyka@wp.pl>
2022-01-03 22:43:29 +0000

    adding scrypt

--- task2/task3.sh
+++ task2/task3.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 2 ]; then
   directory_name=$1
   file_name=$2
-  find $directory_name -type f -empty -delete -fprint $file_name
+  find $directory_name -type d -empty -delete -fprint $file_name
 else
   echo "zla liczba parametrow"    
 fi
---
+++ task2/task8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+  directory_name=$1
+   
+else
+  echo "zla liczba parametrow"    
+fi
+
